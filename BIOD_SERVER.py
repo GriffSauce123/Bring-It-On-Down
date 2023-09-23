7d300c27428dabea9532044ab4b98cae052d6737
GriffSauce123 <71506867+GriffSauce123@users.noreply.github.com>
2023-09-23 19:11:14 -0400

    Update BIOD_SERVER.py

--- BIOD_SERVER.py
+++ BIOD_SERVER.py
@@ -8,39 +8,41 @@ import socket
 import threading
 
 servers = []
-counter = 1
+open_ports = [10001+x for x in range(250)]
+matchmaking = 1
 direct_connections = []
 direct_id = 1111
 
 class Server(object):
 	def __init__(self, ip, port):
 		self.ip = ip
-		self.port = port + counter
+		self.port = port
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.server.bind((self.ip, self.port))
 		self.clients = []
 		self.team = -1
 		
 	def recieve(self):
 		print('RECIEVING\n')
-		global servers, counter, direct_id, direct_connections
+		global servers, matchmaking, direct_id, direct_connections, open_ports
 		while True:
 			try:
 				self.message, self.addr = self.server.recvfrom(64)
 				self.data = self.message.decode()
 
 				if self.data == 'dc':
-					self.clients.remove(self.addr)
+					matchmaking -= len(self.clients)
 					if any(self.addr[0] in sl for sl in direct_connections):
 						for i in range(len(direct_connections)):
 							if self.addr[0] in direct_connections[i]:
 								del direct_connections[i]
-					print(f'[ {self.addr} DISCONNECTED ]')
+					
+					print(f'[ {self.port} DISCONNECTED ]')
 					if len(self.clients) == 2:
 						self.server.sendto(self.message, self.other)
-					elif len(self.clients) == 1:
-						counter -= 1
 					servers.remove(self)
+					open_ports.append(self.port)
 					break
 
 				self.other = []
@@ -68,78 +70,85 @@ class Server(object):
 						print(f'BROADCASTING: {self.data} To {self.other}')
 
 				if self.data == 'tu':
-					if len(self.clients) == 2:
-						self.server.sendto(self.message, self.other)
-						print(f'BROADCASTING: {self.data} To {self.other}')
-					else:
-						print('ONLY ONE CLIENT CONNECTED')
+					self.server.sendto(self.message, self.other)
+					print(f'BROADCASTING: {self.data} To {self.other}')
 
-				if self.data[0] == 'd':
-					for clients in self.clients:
-						self.server.sendto(self.data, client)
+				if self.data[0] == 'd' and self.data[1] != 'c':
+					self.server.sendto(self.message, self.other)
 			
 			except Exception as e:
 				print(e)
+				raise(e)
 
 def main():
-	global servers, counter, direct_id, direct_connections
+	global servers, direct_id, direct_connections, matchmaking
 	main_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ip = '192.168.0.247' # IP OF THE COMPUTER HOSTING THE SERVER
-	port = 10003 # A CLEAR PORT TO USE
+	port = 10000 # A CLEAR PORT TO USE
 	main_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	main_server.bind((ip, port))
+	index = 0
 
 	while True:
 		try:
 			message, addr = main_server.recvfrom(64)
 			data = message.decode()			
-			print(f'Counter: {counter}\nServers: {len(servers)}')
-			#request for direct connection
-			if 'dir' in data:
+			print(f'matchmaking #: {matchmaking}\nServers: {servers}')
+			
+			# --------------------------------------------------------------------------------------------------------- #
+			if 'dir' in data: #request for direct connection
 				#sending an id to the user
 				if len(data) == 3:
 					temp = 'dir' + str(direct_id)
 					main_server.sendto(str(temp).encode(), addr)
-					print(f'DIRECT: {temp}')
 					direct_connections.append([addr[0], addr[1], direct_id])
 					direct_id += 1
-					print(direct_connections)
+					#print(direct_connections)
 
 					s = len(servers)
-					s1 = Server('192.168.0.247', port + 1)
+					s1 = Server('192.168.0.247', open_ports[0])
+					print(open_ports[0])
+					del open_ports[0]
+					print(open_ports[0])
+
 					servers.append(s1)
 					thread = threading.Thread(target=servers[s].recieve)
 					thread.daemon = True
 					thread.start()
 					main_server.sendto((str(servers[s].ip) + '|' + str(servers[s].port)).encode(), addr)
-					counter += 1
 
-				elif len(data) > 3:
+				if len(data) > 3:
 					#check if id match in direct_connections
-					if any(data[3:] in sl for sl in direct_connections):
+					print(f'DATA: {data[3:]}\nCHECK: {any(int(data[3:]) in sl for sl in direct_connections)}\nDirect Conn: {direct_connections}')
+					if any(int(data[3:]) in sl for sl in direct_connections):
+						print('ID IN CONNECTIONS')
+						print(f'ID: {data[:3]}\nConnections: {direct_connections}')
 						for i in range(len(direct_connections)):
 							if data[3:] in direct_connections[i]:
 								index = i
 						main_server.sendto((str(servers[s].ip) + '|' + str(servers[s].port)).encode(), (direct_connections[index][0], direct_connections[index][1]))
 					else:
 						main_server.sendto('dc'.encode(), addr)
-			elif counter % 2 == 0 and len(servers) > 0:
-				#give the current server info (string message will be like '111.111.111.111|4435')
-				s = len(servers)
-				main_server.sendto((str(servers[s - 1].ip) + '|' + str(servers[s - 1].port)).encode(), addr)
-				counter += 1
-				pass
-
-			elif counter % 2 == 1 or len(servers) < 1:
-				#create a new server to provide info for and creates a thread to open the server on
-				s = len(servers)
-				s1 = Server('192.168.0.247', port + 1)
-				servers.append(s1)
-				thread = threading.Thread(target=servers[s].recieve)
-				thread.daemon = True
-				thread.start()
-				main_server.sendto((str(servers[s].ip) + '|' + str(servers[s].port)).encode(), addr)
-				counter += 1
+						
+			# --------------------------------------------------------------------------------------------------------- #
+			
+			if 'G' != data:
+				if matchmaking % 2 == 0 and len(servers) > 0:
+					#give the current server info (string message will be like '111.111.111.111|4435')
+					s = len(servers)
+					main_server.sendto((str(servers[s - 1].ip) + '|' + str(servers[s - 1].port)).encode(), addr)
+					matchmaking += 1
+					
+				elif matchmaking % 2 == 1 or len(servers) < 1:
+					#create a new server to provide info for and creates a thread to open the server on
+					s = len(servers)
+					s1 = Server('192.168.0.247', open_ports[0])
+					servers.append(s1)
+					thread = threading.Thread(target=servers[s].recieve)
+					thread.daemon = True
+					thread.start()
+					main_server.sendto((str(servers[s].ip) + '|' + str(servers[s].port)).encode(), addr)
+					matchmaking += 1
 
 		except Exception as e:
 			print(e)
