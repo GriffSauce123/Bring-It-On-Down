a520612b0f3e2f9a5604bf75947ebfe1e72f1a92
Griffin Adelmann <griffinadelmann@gmail.com>
2024-10-10 19:45:12 -0400

    dice

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const gameboard = document.querySelector(".game-board")
 const player1_segs = document.querySelector(".board-2")
 const player2_segs = document.querySelector(".board-1")
 
+var turn = true //true is player 1, false is player 2
+var num_turns = 0 //number of turns played
+
 for (let seg of player1_segs.children) {
     seg.querySelector(".ring").addEventListener("click", () => {changestate(seg.querySelector(".ring"))})
 }
@@ -12,12 +15,16 @@ for (let seg of player2_segs.children) {
 }
 
 function changestate(element) {
-    if (!(element.style.bottom == "5.1vw")) {
-        element.style.bottom = "5.1vw"
+
+    if (element.parentElement.parentElement.id == "board-1" && turn) { //which board
+
+        if (!(element.style.bottom == "5.1vw")) {
+            element.style.bottom = "5.1vw"
+        }
+        else if (element.style.bottom == "5.1vw") {
+            element.style.bottom = "0"
+        }    
     }
-    else if (element.style.bottom == "5.1vw") {
-        element.style.bottom = "0"
-    }    
 }
 
 function make_safe(element) {
@@ -26,7 +33,22 @@ function make_safe(element) {
 
 function nextturn() {
 
+    turn ++
+
     //push up rings to safe space for other players
+    for (let seg of player1_segs.children) {
+        let ring = seg.querySelector(".ring")
+        if (ring.style.bottom == "5.1vw") {
+            ring.style.bottom = "11.6vw"
+        }
+    }
+
+    for (let seg of player2_segs.children) {
+        let ring = seg.querySelector(".ring")
+        if (ring.style.bottom == "5.1vw") {
+            ring.style.bottom = "11.6vw"
+        }
+    }
 
     if (gameboard.style.rotate == "180deg") {
         gameboard.style.rotate = "0deg"
