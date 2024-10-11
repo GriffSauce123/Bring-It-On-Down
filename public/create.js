568a782f7a49841cd6a83d77e290784ab8c079d9
griffsauce123 <griffinadelmann@gmail.com>
2024-10-11 15:07:10 -0400

    generates codes

--- public/create.js
+++ public/create.js
@@ -1,9 +1,30 @@
 const form_submit = document.getElementById("create-coop")
 const form = document.getElementById("create_form")
+const code_elmnt = document.getElementById("code")
 
 form_submit.addEventListener("click", create_coop_lobby)
 
 function create_coop_lobby() {
-    let code = Math.round(Math.random()*100000,0);
-    console.log(code)
+    
+    //console.log(code)
+
+    //reveal code
+    //send code to backend db so join func works
+    async function getData() {
+        const response = await fetch("/create", {
+            method: "POST",
+        });
+          
+        let data = await response.json()        
+        code_elmnt.innerHTML = data
+        form_submit.innerHTML = "Join"
+        form_submit.removeEventListener("click", create_coop_lobby)
+        form_submit.href = `/join?id=${data}`
+    }
+
+    getData()
+    //change create to join button
+    //go to "waiting for player to join" view
+    
+
 }
--- server.js
+++ server.js
@@ -13,16 +13,25 @@ app.get('/create', (req, res) => {
     res.sendFile(__dirname + '/create.html')
 })
 
+//server creating the code and sending to client
 app.post('/create', (req, res) => {
-    console.log(Math.round(Math.random()*100000,0))
-    res.send(`${Math.round(Math.random()*100000,0)}`)
+    let code = (Math.random() + 'a').slice(2,8);
+    res.send(code)
 })
 
 app.get('/join', (req, res) => {
+    //get id from get request in url and check database
     res.sendFile(__dirname + '/join.html')
 })
 
-
+app.post('/join', (req, res) => {
+    //each game action will be sent here
+    //check state in database ( this needs to be done continuously somehow )
+    //update database
+    //push changes to other player
+    //gives or removes next turn option
+    //if user exits, game is forfeit
+})
 
 //starts the app
 app.listen(10000, function () {
